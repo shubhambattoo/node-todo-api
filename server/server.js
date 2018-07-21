bfb208284384fcac109c0c1182b6b6b08942b1f3
shubham <shubham.battoo@hotmail.com>
2018-07-21 17:36:02 +0530

    delete route added

--- server/server.js
+++ server/server.js
@@ -75,6 +75,29 @@ app.get("/todos/:id", (req, res) => {
 
 })
 
+
+// delete todo
+app.delete("/todos/:id", (req, res) => {
+
+	const id = req.params.id;
+
+	if(!ObjectId.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findByIdAndRemove(id)	
+		.then((todo) => {
+			if(!todo) {
+				return res.status(404).send();
+			}
+
+			res.send({todo})
+		})
+		.catch(e => res.status(400).send())
+
+})
+
+
 app.listen(port);
 console.log("The app was started on " + port);
 
