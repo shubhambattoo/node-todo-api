4efe3e63672e1b6d6870925814ea6f9773ef71f5
shubhambattoo <shubham.battoo@hotmail.com>
2018-11-24 09:48:25 +0530

    changed the mongodb URI

--- server/config/config.js
+++ server/config/config.js
@@ -2,8 +2,8 @@ var env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://shubham:123thedude@ds018848.mlab.com:18848/todoapp2';
+  process.env.MONGODB_URI = 'mongodb://localhost/todoapp2';
 } else if (env === 'test') {
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://shubham:123thedude@ds247101.mlab.com:47101/todoapp2test';
+  process.env.MONGODB_URI = 'mongodb://localhost/todoapp2test';
 }
